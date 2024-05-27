96919b3b809ad5f50c0cf8de3f1f9e0901b39ed4
mr-leo44 <lionnelkaniki44@gmail.com>
2024-05-27 12:45:00 +0200

    feat: update flowbite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ import colors from 'tailwindcss/colors';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
@@ -45,5 +45,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin'),
+    ],
 };
