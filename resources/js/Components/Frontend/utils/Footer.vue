275658374b64bf125047312bd4ee0a6216945be4
mr-leo44 <lionnelkaniki44@gmail.com>
2024-05-27 14:23:06 +0100

    feat: improve footer

--- resources/js/Components/Frontend/utils/Footer.vue
+++ resources/js/Components/Frontend/utils/Footer.vue
@@ -4,20 +4,116 @@
   <footer class="bg-light-secondary dark:bg-dark-secondary py-12">
     <div class="container mx-auto">
       <div
-        class="
-          flex flex-col
-          md:flex-row
-          space-y-6
-          lg:space-y-0
-          items-center
-          justify-between
-        "
+        class="flex flex-col md:flex-row space-y-6 md:space-y-0 items-center justify-between"
       >
         <div class="flex space-x-6 items-center justify-center">
-          
+          <div
+            class="text-light-tail-500 rounded-sm w-14 h-14 flex items-center md:items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl"
+          >
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-8 h-8"
+              >
+                <path
+                  d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div
+            class="text-light-tail-500 rounded-sm w-14 h-14 flex items-center md:items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl"
+          >
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-8 h-8"
+              >
+                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                <path
+                  d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+                ></path>
+                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+              </svg>
+            </a>
+          </div>
+          <div
+            class="text-light-tail-500 rounded-sm w-14 h-14 flex items-center md:items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl"
+          >
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-8 h-8"
+              >
+                <path
+                  d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
+                ></path>
+                <polygon
+                  points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"
+                ></polygon>
+              </svg>
+            </a>
+          </div>
+          <div
+            class="text-light-tail-500 rounded-sm w-14 h-14 flex items-center md:items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl"
+          >
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-8 h-8"
+              >
+                <path
+                  d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                ></path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle>
+              </svg>
+            </a>
+          </div>
+          <div
+            class="text-light-tail-500 rounded-sm w-14 h-14 flex items-center md:items-start justify-center mt-2 mb-4 lg:mb-0 text-2xl"
+          >
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+                stroke-width="1.5"
+                class="w-8 h-8"
+                viewBox="0 0 50 50"
+              >
+                <path
+                  d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"
+                ></path>
+              </svg>
+            </a>
+          </div>
         </div>
         <p class="text-paragraph opacity-80 text-[15px]">
-          &copy; 2022 Laraveller.com All right reserved.
+          &copy; 2024 Tychique Makwela Tous les resérvés.
         </p>
       </div>
     </div>
