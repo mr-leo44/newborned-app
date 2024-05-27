a5d9c5d4bbafbf8cefd05c8b899f059484843cd2
mr-leo44 <lionnelkaniki44@gmail.com>
2024-05-27 12:54:40 +0200

    feat: Promote section

---
+++ resources/js/Components/Frontend/Promote.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-accent dark:bg-dark-navy-500 flex justify-between">
+    <div class="
+          container
+          mx-auto
+          py-12
+          px-4
+          sm:px-6
+          md:flex md:items-center md:justify-between lg:py-16 lg:px-8
+        ">
+      <h2 class="
+            font-bold
+            tracking-tight
+            text-white
+            text-justify
+          dark:text-dark-navy-100
+          ">
+        <span class="block my-4 text-3xl sm:text-4xl">Tout se passe en ligne</span>
+        <span class="block text-lg text-dark-primary dark:text-white text-justify">Finis les déplacements dans les
+          bureaux de l'état civil. Gagnez du temps et simplifiez vos démarches administratives grâce à notre service
+          d'inscription en ligne aux services communaux d'état civil.
+        </span>
+      </h2>
+    </div>
+  </div>
+</template>
