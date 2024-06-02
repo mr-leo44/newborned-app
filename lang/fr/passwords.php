fd34a0658c3608944cbf2e52f1d91bbcde46c815
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-02 19:12:59 +0200

    feat: add French validation

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a bien été réinitialisé.',
+    'sent' => 'Nous vous avons envoyé un lien dans vos mails.',
+    'throttled' => 'Patientez un instant.',
+    'token' => 'Ce code n\'est pas valide.',
+    'user' => "Nous n'avons trouvé aucun utilisateur avec cet email.",
+
+];
