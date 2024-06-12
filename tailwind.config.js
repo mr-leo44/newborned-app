497ee4acc40933006046d8e87ea33e78c056d158
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-12 08:01:08 +0200

    feat:Add Inertia and vue

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import colors from 'tailwindcss/colors';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+        "./node_modules/flowbite/**/*.js",  
+    ],
+
+    theme: {
+        fontFamily: {
+            primary: "Montserrat",
+            body: "Open Sans",
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                lg: "3rem",
+            },
+        },
+        extend: {
+            colors: {
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDFAF6",
+                "dark-primary": "#050402",
+                "dark-secondary": "#1C1D24",
+                "dark-navy-100": "#07567D",
+                "dark-navy-500": "#292D42",
+                accent: {
+                    DEFAULT: "#ac6b34",
+                    hover: "#925a2b",
+                },
+                paragraph: "#87e99",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red,
+                primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        require('flowbite/plugin'),
+    ],
+};
